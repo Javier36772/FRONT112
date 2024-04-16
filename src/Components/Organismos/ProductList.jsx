5d5eb534230d8c936ba0d60fd3672da89f23a8a0
xavi367722 <140395368+xavi367722@users.noreply.github.com>
2024-04-16 14:54:56 -0600

    Add files via upload

---
+++ src/Components/Organismos/ProductList.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import ProductCard from '../Moleculas/ProductCard';
+import './styles/ProductList.css'; 
+import './styles/ProductCategories.css'; 
+import './styles/Styles.css'; 
+
+function ProductList() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/Gamess/games');
+        const data = await response.json();
+        setGames(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (games.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const categories = [...new Set(games.map((game) => game.Precio))];
+
+  return (
+    <div className="custom-product-list">
+      {categories.map((category, categoryIndex) => (
+        <div key={categoryIndex}>
+          <div className="custom-subtitle">{category}</div>
+
+          <div className="custom-product-row">
+            {games
+              .filter((game) => game.Precio === category)
+              .map((game, gameIndex) => (
+                <ProductCard
+                  key={gameIndex}
+                  productName={game.Nombre}
+                  price={game.Precio}
+                  imageSrc={game.Imagen}
+                />
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
